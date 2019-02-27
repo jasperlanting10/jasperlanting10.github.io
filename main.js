5a4148a21f90ebc899fa4436e3ad676ac066bcca
jasperlanting <jasper.lanting10@gmail.com>
2019-02-27 20:18:48 +0100

    changed for test

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function makeMain(){
 
-    let main = document.createElement('main');
-    let title = document.createElement('title');
+    let main = document.createElement("main");
+    let title = document.createElement("title");
 
     title.innerText("Hallo & welkom op mijn site!!");
 
-    let input = document.createElement('input');
-    let button = document.createElement('button');
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder","Enter your name brotha");
+
+
+    let button = document.createElement("input");
+    button.setAttribute("type", "button");
+
 
 }
