fb58e32fb2321ca3c4a193a37a2eeda3b8d6e567
jasperlanting <jasper.lanting10@gmail.com>
2019-02-28 15:13:39 +0100

    big difference

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 
-function makeMain(){
-    alert('hoi');
-    let main = document.createElement("main");
-    let h1 = document.createElement("h1");
-    h1.innerText("Hallo & welkom op mijn site!!");
+function makeMain(name){
+    alert(name.value + ' is gay!');
 
-    let input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("placeholder","Enter your name brotha");
-
-
-    let button = document.createElement("input");
-    button.setAttribute("value", "Add");
-    button.setAttribute("type", "button");
-
-    button.addEventListener("click", function(){
-        h1.innerText(input.value + " is gay!");
-    });
 
 }
 
